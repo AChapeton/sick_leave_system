b9a4b2506a745f51520b7fbb79fc1a287e493c1b
Andres Chapeton <andres.chapeton0206@gmail.com>
2023-03-15 10:48:44 -0600

    Basic router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./modules/LoginPage";
+import { HomePage } from "./modules/HomePage";
+import { NewApplicationPage } from "./modules/NewApplicationPage";
+
 function App() {
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/new_application" element={<NewApplicationPage />} />
+          <Route path="*" element={<p>Not Found</p>} />
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
---
+++ src/modules/NewApplicationPage/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewApplicationPage = () => {
+  return <div> NewApplicationPage</div>;
+};
+
+export { NewApplicationPage };
