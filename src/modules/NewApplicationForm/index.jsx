d76e509b07060c8ffc081632e1bdad3126506af3
Andres Chapeton <andres.chapeton0206@gmail.com>
2023-03-17 11:45:23 -0600

    Required each input from new application form

--- src/modules/NewApplicationForm/index.jsx
+++ src/modules/NewApplicationForm/index.jsx
@@ -1,31 +1,87 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const NewApplicationForm = () => {
+  const {
+    register,
+    reset,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = async (applicationData) => {
+    console.log(applicationData);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label>Employee</label>
-        <input type="text" name="employeeName" id="employeeName" />
+        <input
+          type="text"
+          name="employeeName"
+          id="employeeName"
+          {...register("employeeName", { required: true })}
+        />
+        {errors.employeeName?.type === "required" && (
+          <p>Employee's name is required</p>
+        )}
       </div>
       <div>
         <label>Medical unit</label>
-        <input type="text" name="medicalUnit" id="medicalUnit" />
+        <input
+          type="text"
+          name="medicalUnit"
+          id="medicalUnit"
+          {...register("medicalUnit", { required: true })}
+        />
+        {errors.medicalUnit?.type === "required" && (
+          <p>Medical unit is required</p>
+        )}
       </div>
       <div>
         <label>Doctor</label>
-        <input type="text" name="doctorName" id="doctorName" />
+        <input
+          type="text"
+          name="doctorName"
+          id="doctorName"
+          {...register("doctorName", { required: true })}
+        />
+        {errors.doctorName?.type === "required" && (
+          <p>Doctor's name is required</p>
+        )}
       </div>
       <div>
         <label>Days of coverage</label>
-        <input type="number" name="coverageDays" id="coverageDays" />
+        <input
+          type="number"
+          name="coverageDays"
+          id="coverageDays"
+          {...register("coverageDays", { required: true })}
+        />
+        {errors.coverageDays?.type === "required" && (
+          <p>Coverage days are required</p>
+        )}
       </div>
       <div>
         <label>Sick leave start date</label>
-        <input type="text" name="startDate" id="startDate" />
+        <input
+          type="date"
+          name="startDate"
+          id="startDate"
+          {...register("startDate", { required: true })}
+        />
+        {errors.startDate?.type === "required" && <p>Start date is required</p>}
       </div>
       <div>
         <label>Sick leave end date</label>
-        <input type="text" name="endDate" id="endDate" />
+        <input
+          type="date"
+          name="endDate"
+          id="endDate"
+          {...register("endDate", { required: true })}
+        />
+        {errors.endDate?.type === "required" && <p>End date is required</p>}
       </div>
       <div>
         <label>Medical diagnostic</label>
@@ -34,7 +90,11 @@ const NewApplicationForm = () => {
           id="medicalDiagnostic"
           cols="30"
           rows="10"
+          {...register("medicalDiagnostic", { required: true })}
         ></textarea>
+        {errors.medicalDiagnostic?.type === "required" && (
+          <p>Medical diagnostic is required</p>
+        )}
       </div>
       <button>Submit application</button>
     </form>
