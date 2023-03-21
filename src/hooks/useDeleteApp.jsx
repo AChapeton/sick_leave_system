04d996b828539e6e67e2eed09bfed08368c6d53e
Andres Chapeton <andres.chapeton0206@gmail.com>
2023-03-21 17:12:47 -0600

    useDeleteApp file with comments

--- src/hooks/useDeleteApp.jsx
+++ src/hooks/useDeleteApp.jsx
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { deleteApplication } from "../useContentful";
 
+//Function that calls Contentful logic
 const deleteApp = async (row) => {
   deleteApplication(row.sysId);
   return null;
 };
 
+//Function that manage the data
 const useDeleteApp = (row) => {
-  return useQuery(["apps", row], () => deleteApp(row));
+  return useQuery(["app", row], () => deleteApp(row));
 };
 
 export { useDeleteApp };
