8597cafb853a788ae74758a1788c8967c6f9a6d7
Andres Chapeton <andres.chapeton0206@gmail.com>
2023-03-22 15:23:04 -0600

    Home styles

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useLogin } from "../../hooks/store";
 import { ApplicationsTable } from "../../modules/ApplicationsTable";
 import { NavLink, useNavigate } from "react-router-dom";
+import styles from "./styles.module.scss";
+import profileImg from "../../assets/photo.svg";
+import logoutIcon from "../../assets/fi_log-out.svg";
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -16,10 +19,26 @@ const HomePage = () => {
   };
 
   return (
-    <div>
-      <NavLink to="/profile">{loggedUser[0].username}</NavLink>
-      <button onClick={onHandleLogout}>Log Out</button>
-      <NavLink to="/new_application">New application</NavLink>
+    <div className={styles.home}>
+      <div className={styles.home__header}>
+        <div className={styles.home__header_profile}>
+          <img src={profileImg} alt="Profile photo" />
+          <div className={styles.home__actions}>
+            <NavLink to="/profile">
+              <h3>{loggedUser[0].username}</h3>
+            </NavLink>
+            <button onClick={onHandleLogout}>
+              <img src={logoutIcon} alt="Log out icon" />
+              <span>Log Out</span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <NavLink to="/new_application">
+            <button className={styles.home__button}>New application</button>
+          </NavLink>
+        </div>
+      </div>
       <ApplicationsTable />
     </div>
   );
